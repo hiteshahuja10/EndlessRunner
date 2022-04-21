5300ce96304b75f7b0f39c3e9da591643a700fe1
“Hitesh <hahuja@ucsc.edu>
2022-04-21 10:09:32 -0700

    Merge branch 'main' of https://github.com/hiteshahuja10/EndlessRunner into main

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,23 +4,23 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('beach', './assets/tile.png');
+        this.load.image('tile', './assets/tile.png');
         //this.load.spritesheet('explosion', './assets/sharkexplosion.png', {frameWidth: 64, frameHeight: 32, 
             //startFrame: 0, endFrame: 6});
     }
 
     create() {
         // place tile sprite
-        this.beach = this.add.tileSprite(0, 0, 640, 480, 'beach').setOrigin(0, 0);
+        this.beach = this.add.tileSprite(0, 0, 640, 480, 'tile').setOrigin(0, 0);
         // blue UI background
         this.add.rectangle(0, borderUISize, game.config.width, (borderUISize * 2)-5, 
-            0x00EED1).setOrigin(0, 0);
+            0x000000).setOrigin(0, 0);
         // purple borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x9fa8da).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 
-            0x9fa8da).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0x9fa8da).setOrigin(0, 0);
+            0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height,
-            0x9fa8da).setOrigin(0, 0);
+            0xFFFFFF).setOrigin(0, 0);
     }  
 }
