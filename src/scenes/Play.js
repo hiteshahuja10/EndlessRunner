b0b57e719ed8993f1f9ea095c6ad41860c5e6e5e
Nicholas <nklau@ucsc.edu>
2022-04-21 14:19:32 -0700

    spikes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,12 +2,14 @@ class Play extends Phaser.Scene {
     constructor(){
         super("playScene");
         this.platforms;
+        this.spikes;
     }
 
     preload() {
         this.load.image('tile', './assets/tile.png');
         this.load.image('platform', './assets/StonePlatform.png');
         this.load.image('player', './assets/sharknew.png');
+        this.load.image('spike','./assets/Spikes.png')
         //this.load.spritesheet('explosion', './assets/sharkexplosion.png', {frameWidth: 64, frameHeight: 32, 
             //startFrame: 0, endFrame: 6});
     }
@@ -30,33 +32,20 @@ class Play extends Phaser.Scene {
         this.player.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.player.jump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.createPlatform();
-<<<<<<< HEAD
-        this.physics.add.collider(player, platforms);
-        
-=======
         this.physics.add.collider(this.player, this.platforms); 
->>>>>>> 4bdf4f4fce6a6b3ace32ee4b77386dd946a45ef3
+        this.physics.add.collider(this.player, this.spikes);
     }
     
     update(){
         this.tile.tilePositionY -= 4;
-<<<<<<< HEAD
-=======
         
         
->>>>>>> 4bdf4f4fce6a6b3ace32ee4b77386dd946a45ef3
     }
 
     createPlatform(){
         this.platforms = this.physics.add.staticGroup();
+        this.spikes = this.physics.add.staticGroup()
         this.platforms.create(600,400, 'platform').setScale(2).refreshBody();
-<<<<<<< HEAD
-=======
-        
->>>>>>> 4bdf4f4fce6a6b3ace32ee4b77386dd946a45ef3
-
-
-//this.platforms.add('platform');
-        //this.platforms.setscale(2);
+        this.spikes.create(400,400,'spike').setScale(2).refreshBody();
     }
 }
