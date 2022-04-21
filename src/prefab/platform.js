1af942205056f66ad7abec7ca41c0ed83231721d
Nicholas <nklau@ucsc.edu>
2022-04-20 19:24:36 -0700

    commit 'platform'

---
+++ src/prefab/platform.js
@@ -0,0 +1,13 @@
+class Platform extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y,texture,frame){
+        super(scene,x,y,texture,frame);
+
+        scene.add.existing(this);
+
+
+    }
+
+    update(){
+
+    }
+}
