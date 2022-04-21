abd454cf4400d92ebfb9c44b3401d8bb9cbc6ff4
datguyandres <spartansup@gmail.com>
2022-04-21 10:28:27 -0700

    update player file

--- src/prefab/dude.js
+++ src/prefab/dude.js
@@ -1,9 +1,34 @@
 class dude extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture) {
-        super(scene, x, y, texture)
-        scene.add.existing(this)
-        scene.physics.add.existing(this)
+        super(scene, x, y, texture);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.left;
+        this.right;
+        this.jump;
     
-        this.setCollideWorldBounds(true)
+        this.setCollideWorldBounds(true);
     }
+
+  update(){
+    if(this.left.isDown){
+        this.setVelocityX(-160);
+
+        //add animation line here for when facing left
+    }
+    if(this.right.isDown){
+        this.setVelocityX(160);
+
+        //add animation line here for when facing left
+    }
+
+    if(Phaser.Input.Keyboard.JustDown(jump) && this.body.touching.down ){ // add checking to see if its on the floor befoe jumping or else it will keep jumping
+        this.setVelocityY(-330);
+
+        //add animation for jumping here
+    }
+
+  }
+
 }
