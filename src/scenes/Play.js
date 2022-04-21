2440dd0348a2eaa1462f4817e9905cfc074c8e86
Andres Benitez <spartansup18@gmail.com>
2022-04-21 14:03:56 -0700

    collision

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -28,6 +28,7 @@ class Play extends Phaser.Scene {
         this.player.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.player.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.player.jump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.physics.add.collider(this.player, this.platforms);
     }
     
     update(){
