a076ab3e8bbf7eace57d250adae32ac1601a9834
datguyandres <spartansup@gmail.com>
2022-04-21 10:12:58 -0700

    added character file

---
+++ src/prefab/dude.js
@@ -0,0 +1,9 @@
+class dude extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture)
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+    
+        this.setCollideWorldBounds(true)
+    }
+}
